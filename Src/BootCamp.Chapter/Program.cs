c74a86e67a6a5f6462403a6fedf451a9d9963ff0
dchio <achiorean.mail@gmail.com>
2020-08-16 14:59:56 +0200

    Add descriptive line about person + other minor tweaks

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -7,24 +7,26 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your first name: ");
+            Console.WriteLine("Please enter a first name: ");
             string firstName = Console.ReadLine();
 
-            Console.WriteLine("Please enter your first name: ");
+            Console.WriteLine("Please enter a last name: ");
             string lastName = Console.ReadLine();
 
-            Console.WriteLine("Please enter your age: ");
+            Console.WriteLine("Please enter an age: ");
             var age = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter your height in meters: ");
+            Console.WriteLine("Please enter a height in meters: ");
             var height = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Please enter your weight in kilograms: ");
+            Console.WriteLine("Please enter a weight in kilograms: ");
             var weight = double.Parse(Console.ReadLine());
 
+            Console.WriteLine($"{firstName} {lastName} is {age} years old, his weight is {weight} kg and his height is {height * 100} meters.");
+                        
             var bmi = weight / (height * height);
 
-            Console.WriteLine(Math.Round(bmi, 2));
+            Console.WriteLine($"Their BMI is: {Math.Round(bmi, 2)}.");
         }
     }
 }
