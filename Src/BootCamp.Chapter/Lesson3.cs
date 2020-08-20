609ebd713833b1742c7873589b0da494ab3161e3
dchio <achiorean.mail@gmail.com>
2020-08-20 10:48:05 +0200

    Add prompt methods and start calling them

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
+using System.Threading.Channels;
 
 namespace BootCamp.Chapter
 {
     class Lesson3
     {
+        static void Demo()
+        {
+            string questionMessage = "What is your ";
+
+            var firstName = PromptString(questionMessage + "first name");
+            var lastName = PromptString(questionMessage + "lirst name");
+
+
+
+        }
+
+        static int PromptInt(string message)
+        {
+            Console.WriteLine(message + "?");
+            var input = int.Parse(Console.ReadLine());
+                        
+            return input;
+        }
+
+        static string PromptString(string message)
+        {
+            Console.WriteLine(message + "?");
+            var input = Console.ReadLine();
+
+            return input;
+        }
+
+        static float PromptFloat(string message)
+        {
+            Console.WriteLine(message + "?");
+            var input = float.Parse(Console.ReadLine());
+
+            return input;
+        }
+
+
     }
 }
