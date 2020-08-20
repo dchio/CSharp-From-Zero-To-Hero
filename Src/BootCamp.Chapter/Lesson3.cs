34b09aa4ce0ca9da019f8ded21ab3daafc0eeb09
dchio <achiorean.mail@gmail.com>
2020-08-20 12:11:54 +0200

    Add code from last homework

---
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace BootCamp.Chapter
+{
+    class Lesson3
+    {
+        public static void Demo()
+        {
+            string questionMessage = "What is your ";
+
+            var firstName = PromptString(questionMessage + "first name");
+            var lastName = PromptString(questionMessage + "lirst name");
+
+            var age = PromptInt(questionMessage + "age");
+
+            var weight = PromptFloat(questionMessage + "weight in kg");
+            var height = PromptFloat(questionMessage + "height in meters");
+
+            var bmi = CalculateBmi(weight, height);
+
+            Console.WriteLine($"{firstName} {lastName} is {age} years old, his weight is {weight} kg and his height is {height} m.");
+            Console.WriteLine($"His BMI is: {bmi}.");
+
+        }
+
+        public static int PromptInt(string message)
+        {
+            Console.WriteLine(message);
+            var input = int.Parse(Console.ReadLine());
+
+            return input;
+        }
+
+        public static string PromptString(string message)
+        {
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+
+            return input;
+        }
+
+        public static float PromptFloat(string message)
+        {
+            Console.WriteLine(message);
+            var input = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            return input;
+        }
+
+        public static float CalculateBmi(float weight, float height)
+        {
+            var bmi = weight / (height * height);
+
+            return bmi;
+        }
+
+    }
+}
