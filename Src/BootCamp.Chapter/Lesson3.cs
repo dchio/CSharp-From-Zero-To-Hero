bdcea00eb660cf8284f91794199b5ec1bbb41d8e
dchio <achiorean.mail@gmail.com>
2020-08-20 10:56:14 +0200

    Finish Lesson3.cs and call demo method in Program.cs

--- Src/BootCamp.Chapter/Lesson3.cs
+++ Src/BootCamp.Chapter/Lesson3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Channels;
@@ -8,14 +9,22 @@ namespace BootCamp.Chapter
 {
     class Lesson3
     {
-        static void Demo()
+        public static void Demo()
         {
             string questionMessage = "What is your ";
 
             var firstName = PromptString(questionMessage + "first name");
             var lastName = PromptString(questionMessage + "lirst name");
 
+            var age = PromptInt(questionMessage + "age");
 
+            var weight = PromptFloat(questionMessage + "weight in kg");
+            var height = PromptFloat(questionMessage + "height in meters");
+
+            var bmi = CalculateBmi(weight, height);
+
+            Console.WriteLine($"{firstName} {lastName} is {age} years old, his weight is {weight} kg and his height is {height} m.");
+            Console.WriteLine($"His BMI is: {bmi}.");
 
         }
 
@@ -41,6 +50,13 @@ namespace BootCamp.Chapter
             var input = float.Parse(Console.ReadLine());
 
             return input;
+        }
+
+        static float CalculateBmi(float weight, float height)
+        {
+            var bmi = weight / (height * height);
+
+            return bmi;
         }
 
 
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -9,28 +9,7 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a first name: ");
-            string firstName = Console.ReadLine();
-
-            Console.WriteLine("Please enter a last name: ");
-            string lastName = Console.ReadLine();
-
-            Console.WriteLine("Please enter an age: ");
-            var age = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please enter a height in meters: ");
-            var height = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Please enter a weight in kilograms: ");
-            var weight = double.Parse(Console.ReadLine());
-
-            Console.WriteLine($"{firstName} {lastName} is {age} years old, his weight is {weight} kg and his height is {height * 100} centimeters.");
-
-            // formula that is used to calculate the bmi
-            var bmi = weight / (height * height);
-
-            // output rounded bmi - rounding is happening to make number look nicer (xx.xx rather than xx.xxxxxxxx)
-            Console.WriteLine($"Their BMI is: {Math.Round(bmi, 2)}.");
+            Lesson3.Demo();
         }
     }
 }
